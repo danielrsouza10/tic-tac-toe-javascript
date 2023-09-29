70a03fe9345f084cf6626439a304f98991401ad7
Daniel Souza <danielrsouza10@gmail.com>
2023-09-29 09:22:32 -0300

    fetch modifications

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const gameBoard = document.querySelectorAll(".box");
 
+let playerOne = 0;
+let playerTwo = 1;
+
 gameBoard.forEach((box) => {
-  box.addEventListener("click", () => (box.innerHTML = "x"));
+  box.addEventListener("click", () => {
+    if (playerOne % 2 == 0) {
+      box.innerHTML = "X";
+      playerOne++;
+      playerTwo++;
+    } else {
+      box.innerHTML = "O";
+      playerOne++;
+      playerTwo++;
+    }
+  });
 });
 
 const player = () => {
