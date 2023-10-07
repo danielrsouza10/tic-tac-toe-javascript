142f1f2cc91156197ea7640a0aed074b9546f645
Daniel Souza <danielrsouza10@gmail.com>
2023-10-07 19:36:36 -0300

    far from done

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
-const gameBoard = document.querySelectorAll(".box");
+const gameBoard = {
+  lines: 3,
+  columns: 3,
+};
 
-let playerOne = 0;
-let playerTwo = 1;
+const container = document.querySelector(".container");
 
-gameBoard.forEach((box) => {
+for (i = 0; i < 9; i++) {
+  const box = document.createElement("div");
+  box.innerHTML = "";
+  box.className = "box";
+  box.dataset.index = i;
+  container.appendChild(box);
+  box.addEventListener("click", (e) =>
+    console.log(e.target.attributes["data-index"].value)
+  );
+}
+
+function verificarVencedor() {}
+
+const clearButton = document.getElementById("reset-button");
+clearButton.addEventListener("click", () =>
+  gameBoard.forEach((box) => {
+    box.innerHTML = "";
+  })
+);
+
+console.log(gameBoard.lines);
+
+// const gameCel = document.createElement("div");
+// gameCel.className = "box";
+
+// function createGameBoard() {}
+
+// let playerOne = 0;
+// let playerTwo = 1;
+
+container.forEach((box) => {
   box.addEventListener("click", () => {
     if (playerOne % 2 == 0) {
       box.innerHTML = "X";
@@ -17,6 +49,8 @@ gameBoard.forEach((box) => {
   });
 });
 
-const player = () => {
-  let points = 0;
-};
+// function verifyRow() {}
+
+// const player = () => {
+//   let points = 0;
+// };
